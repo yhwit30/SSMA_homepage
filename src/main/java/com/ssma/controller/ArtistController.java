91076a2c4f21c5a289db49f3092bdca77da96622
yhwit30 <yhwit30@gmail.com>
2024-06-26 15:47:20 +0900

    shop, 강사진 주석처리 등 정리

--- src/main/java/com/ssma/controller/ArtistController.java
+++ src/main/java/com/ssma/controller/ArtistController.java
@@ -1,24 +1,15 @@
 package com.ssma.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.ssma.service.ArtistService;
-import com.ssma.vo.Artist;
 
 @Controller
 public class ArtistController {
 
 
-	@Autowired
-	private ArtistService artistService;
 
-
-	@RequestMapping("artist/detail")
+	@RequestMapping("/artist/detail")
 	public String getArtist(Model model) { 
 
 		
@@ -26,12 +17,8 @@ public class ArtistController {
 	}
 
 
-	@RequestMapping("artist/list")
-	public String showList(Model model) {
-	List<Artist> artists = artistService.getArtists();
-		
-		
-		model.addAttribute("artists", artists);
+	@RequestMapping("/artist/list")
+	public String showList() {
 
 		return "content/artist";
 	}
